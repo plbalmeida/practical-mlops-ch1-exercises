03d4d4f27d4f25561b010e84024e82cf2cf313ce
plbalmeida <plbalmeida@usp.br>
2024-01-25 00:23:29 -0300

    fix(./tests/test_main.py): tests improved

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,32 @@
-from src.main import add
+import pytest
+from src.main import app, add
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
 
 
 def test_add():
-    assert 2 == add(1, 1)
+    assert add(1, 1) == 2
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+
+
+def test_index_post(client):
+    response = client.post('/', json={'x': 1, 'y': 2})
+    assert response.status_code == 200
+    assert response.json['result'] == 3
+
+
+def test_index_post_invalid_data(client):
+    response = client.post('/', json={'a': 1, 'b': 2})
+    assert response.status_code == 200
+    assert response.json['result'] is None
+
+
+def test_index_post_missing_data(client):
+    response = client.post('/', json={'x': 1})
+    assert response.status_code == 200
+    assert response.json['result'] is None
